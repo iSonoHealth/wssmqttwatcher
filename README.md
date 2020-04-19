456ed7a5e7e596bfc3134bb5f14f7f1017f0ce1e
Ray Hu <ray@isonohealth.com>
2020-04-18 22:16:12 -0700

    added npm modules
    Added argv, valid-url, lint, prettier, nodemon, js-yaml

---
+++ README.md
@@ -0,0 +1,1 @@
+A simple Websockets MQTT Watcher
--- index.js
+++ index.js
@@ -1,26 +1,49 @@
+#!/usr/bin/env node
+
 const path = require('path')
 const configFileFullName = path.join(__dirname, 'config.yml')
 const config = require('./configuration').getAll(configFileFullName) // Log Start Message
 
-const mqtt = require("mqtt")
-const client = mqtt.connect(config.server_url)
+const argv = require('yargs').usage(
+    '$0 <url>',
+    'A simple WebSockets MQTT Watcher',
+    (yargs) => {
+        yargs.positional('url', {
+            describe: `the url you want to watch, set <url> to local will watch ${config.server_url}`,
+            type: 'string',
+        })
+    }
+).argv
+
+let url = argv.url
+
+const validUrl = require('valid-url')
+if (!validUrl.isUri(url)) {
+    if (url === 'local') {
+        url = config.server_url
+    } else {
+        console.log('Provided url is not valid')
+        process.exit(1)
+    }
+}
+
+const mqtt = require('mqtt')
+const client = mqtt.connect(url)
 client.on('connect', function () {
-    client.subscribe("#", function (err) {
+    client.subscribe('#', function (err) {
         if (!err) {
-            client.publish(`WebSockets MQTT Watcher ${
-                config.send_version
-                    ? `version: ${config.version}`
-                    : ''
-                }`, 'Hello')
+            client.publish(
+                `WebSockets MQTT Watcher ${
+                    config.send_version ? `version: ${config.version}` : ''
+                }`,
+                'Hello'
+            )
         }
     })
 })
 
 client.on('message', function (topic, message) {
     // message is Buffer
-    console.log(`Received message of topic:  ${topic}, size: ${message.length} `)
+    console.log(`Received message. Topic:  ${topic} Size: ${message.length} `)
     //console.log(message.toString())
-    //client.end()
 })
-
-
